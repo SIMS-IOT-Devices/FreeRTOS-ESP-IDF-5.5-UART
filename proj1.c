d8fa27b17c2ad598e46f96422756ade6c37f0c0a
SIMS IOT Devices <92898043+SIMS-IOT-Devices@users.noreply.github.com>
2026-01-09 16:13:08 +0200

    Add files via upload

---
+++ proj1.c
@@ -0,0 +1,37 @@
+#include "driver/uart.h"
+#include "esp_log.h"
+#include <string.h>
+
+#define UART_PORT UART_NUM_0
+#define BUF_SIZE 128
+
+static const char *TAG = "UART_RX";
+
+void app_main(void)
+{
+    uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity    = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
+    };
+
+    uart_param_config(UART_PORT, &uart_config);
+    uart_driver_install(UART_PORT, BUF_SIZE * 2, 0, 0, NULL, 0);
+    uint8_t data[BUF_SIZE];
+
+    while (1) {
+        int len = uart_read_bytes(
+            UART_PORT,
+            data,
+            BUF_SIZE - 1,
+            pdMS_TO_TICKS(1000)
+        );
+
+        if (len > 0) {
+            data[len] = '\0';  
+            ESP_LOGI(TAG, "Received: %s", (char *)data);
+        }
+    }
+}
